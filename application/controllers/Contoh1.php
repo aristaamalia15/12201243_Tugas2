bfb4e74521635e9ee9c6724ccd9e5b39666427b3
aristaamalia15 <arista.amalia25@gmail.com>
2021-09-23 19:31:37 +0700

    menambahkan tugas 2 dan biodata

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo"<h1>Perkenalkan</h1>";
+		echo"Nama saya Arista Amalia Agustin.
+			Saya tinggal di Jl. Tangsi RT 009/ RW 002, Desa Margaluyu, Kecamatan Manonjaya,Tasikmalaya.
+			Hobi saya adalah menulis dan membaca, Nomor Hp saya yaitu 085721820389 ";
+	}
+}
